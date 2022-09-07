b4c20766e006866b7cbb6ed2d82f9cd7c06738b7
SumairAlamGD <90307817+SumairAlamGD@users.noreply.github.com>
2022-09-07 16:03:19 +0500

    Updated label property

--- src/Components/Form/Properties/Columns/Accordion/index.jsx
+++ src/Components/Form/Properties/Columns/Accordion/index.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import {useState} from 'react';
+import {
+    singleDataChange
+} from '../../../FunctionalData'
 
 export default function Accordion(props) {
+    const [name, setName] = useState(props?.data?.label);
     return (
         <div className='properties'>
             <span className='properties-header'>Properties</span>
             <div visibility={props?.visibility?.toString()} className='properties-components'>
                 <h3 style={{ textAlign: "center" }}>Accordion</h3>
                 <span>Name:</span>
-                <input value={props?.inputFieldLabel} onChange={props?.handleName} type="text" />
+                <input value={name} onChange={(e) => {setName(e.target.value); singleDataChange(e.target.value, props?.data?.id, "label", props?.elements, props?.setElements);}} type="text" />
                 <span>Type:</span>
                 <select onChange={props?.handleSelectChange}>
                     {props?.nd?.inputTypes.map((it) => {
--- src/Components/Form/Properties/Columns/ToggleButton/index.jsx
+++ src/Components/Form/Properties/Columns/ToggleButton/index.jsx
@@ -1,19 +1,17 @@
-import React from 'react';
+import {useState} from 'react';
+import {
+    singleDataChange
+} from '../../../FunctionalData'
 
 export default function ToggleButton(props) {
+    const [name, setName] = useState(props?.data?.label);
     return (
         <div className='properties'>
             <span className='properties-header'>Properties</span>
             <div visibility={props?.visibility?.toString()} className='properties-components'>
                 <h3 style={{ textAlign: "center" }}>Toggle Button</h3>
                 <span>Name:</span>
-                <input value={props?.inputFieldLabel} onChange={props?.handleName} type="text" />
-                <span>Type:</span>
-                <select onChange={props?.handleSelectChange}>
-                    {props?.nd?.inputTypes.map((it) => {
-                        return <option key={it?.id} value={it?.type.toLowerCase()}>{it?.type}</option>
-                    })}
-                </select>
+                <input value={name} onChange={(e) => {setName(e.target.value); singleDataChange(e.target.value, props?.data?.id, "label", props?.elements, props?.setElements);}} type="text" />
             </div>
         </div>
     )
